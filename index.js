f01ca82c3cd1fa62bdf665ca2f235560e325a3ab
jessicashinzato <jessicagouveasilva@icloud.com>
2025-06-10 14:28:38 +0900

    adicionei o texto

---
+++ index.js
@@ -0,0 +1,19 @@
+let nomeHeroi = ("Junior")
+
+let xp = ("8050")
+
+
+switch (xp){ 
+    
+    case "2000":
+    console.log( "Xp Prata") 
+    break
+
+    case "7000": 
+    console.log("XP Ouro")
+    break
+
+    case "8050":
+    console.log(" Junior esta com 8050 portando é um jogador nivel Ascendente")
+    break
+}
